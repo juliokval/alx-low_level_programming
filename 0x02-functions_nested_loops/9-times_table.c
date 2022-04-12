67fba8b29bd7c1f0b2a6a0ec3ccb3a4a58f32e86
juliokval <julesvalentin04@gmail.com>
2022-04-12 06:54:00 -0700

    From0-11

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,14 +7,14 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i =0; i<=9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
 			if (j == 0)
 			{
-				_putchar(k + '0')
+				_putchar(k + '0');
 			}
 			else if (k <= 9)
 			{
@@ -28,7 +28,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0')
+				_putchar(k % 10 + '0');
 			}
 		}
 		_putchar('\n');
