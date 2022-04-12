e51b0f411486485316dfad93ce42b429a5073924
juliokval <julesvalentin04@gmail.com>
2022-04-12 04:52:16 -0700

    From0-11

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
-  * _islower - returns 1 if lowercase alphabet and o if not
-  * @c: takes in a character
-  * Return: 0 for uppercase, 1 for lowercase
-  */
+ * _islower - returns 1 if lowercase alphabet and o if not
+ * @c: takes in a character
+ * Return: 0 for uppercase, 1 for lowercase
+ */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
