3553c76af51b683760b0dfc9c34d3076bb8c26f7
juliokval <julesvalentin04@gmail.com>
2022-04-20 09:45:15 -0700

    From 0-9

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-  * _puts - function that prints a string, followed by a new line, to stdout
-  *
-  * @str: A pointer to an int that will be changed
-  *
-  * Return: void which means the answer is correct 
-  */
+ * _puts - function that prints a string, followed by a new line, to stdout
+ *
+ * @str: A pointer to an int that will be changed
+ *
+ * Return: void which means the answer is correct.
+ */
 
 void _puts(char *str)
 {
