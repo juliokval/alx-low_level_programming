edb3a7a226a5981725b9b8dcef7dcd0c16373c14
juliokval <julesvalentin04@gmail.com>
2022-04-20 14:57:32 -0700

    From 100-101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -21,7 +21,7 @@ int main(void)
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
-		_putchr(pass[i] + '0');
+		_putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
