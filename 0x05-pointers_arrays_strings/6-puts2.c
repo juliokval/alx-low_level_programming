fbabf6cc68ce7d309f602b4ff16a3abdcb3dd68c
juliokval <julesvalentin04@gmail.com>
2022-04-20 11:15:09 -0700

    From 0-9

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-  * puts2 - function that prints every other character of a string, starting with the first character
-  * followed by a new line
+  * puts2 - function that prints every other character of a string
+  * starting withthe first character, followed by a new line
   *@str: input string
   * Return: no return
   */
