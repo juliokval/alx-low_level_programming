53c60573b51a5058c79812ddb58e4d553992b9f7
juliokval <julesvalentin04@gmail.com>
2022-04-20 10:18:28 -0700

    From 0-9

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_rev - prints a string in reverse order
- *@S: A pointer to an int that will be changed
+ *@s: A pointer to an int that will be changed
  *
  * Return: void which means our answer is correct
  */
