1195d13ed35cc389e835d1c672941e4a8897ab5a
juliokval <julesvalentin04@gmail.com>
2022-04-28 17:57:35 -0700

    From 100-101

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * _strlem_recursion - returns the length of a string
+  * _strlen_recursion - returns the length of a string
   * @s: string
   * Return: the length of a string
   */
