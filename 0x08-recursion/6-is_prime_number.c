c8d218bedc90f03c15dbef339368bb08b7ecdd7c
juliokval <julesvalentin04@gmail.com>
2022-04-28 17:29:09 -0700

    From 0-6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int is_prime(unsigned int n, unsigned int c)
 	{
 		if (n == 0)
 			return (1);
-		else 
+		else
 			return (0);
 	}
 	return (0 + is_prime(n, c + 1));
@@ -20,7 +20,7 @@ int is_prime(unsigned int n, unsigned int c)
 /**
   * is_prime_number - detects if an input number is a prime number
   * @n: input number
-  * Return: 1 id n is a prime number, 0 if n is not a prime number
+  * Return: 1 if n is a prime number, 0 if n is not a prime number
   */
 int is_prime_number(int n)
 {
