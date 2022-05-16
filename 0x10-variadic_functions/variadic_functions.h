d47873cd647f04b5fab6f8f2e260234913a3d7d2
juliokval <julesvalentin04@gmail.com>
2022-05-16 10:32:23 -0700

    FROM 0-4

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef _VARIADIC_H_
-#define _VARIADIC_H_
+#ifndef _FUNCTION_VARIADIC_H_
+#define _FUCNCTION_VARIADIC_H_
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
