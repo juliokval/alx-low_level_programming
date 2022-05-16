b149b3c5bf8444b9df4de4fa1d48e751fa204938
juliokval <julesvalentin04@gmail.com>
2022-05-16 10:37:48 -0700

    FROM 0-4

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,14 +1,12 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-
 /**
  * print_numbers - print variadic number
  * @separator: char
  * @n:variadic variable count
  * Return :void
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
