eaeb4e51d3cf0143ba6a6d9c344d787a1cc78d76
juliokval <julesvalentin04@gmail.com>
2022-05-24 06:57:37 -0700

    FROM 0-6

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
 * print_list - prints all the elements of a list_t list.
-* @h: linked list.
+* @h: singly linked list.
 * Return: number of elements in the list.
 */
 size_t print_list(const list_t *h)
