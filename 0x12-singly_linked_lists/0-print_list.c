aa651d57bf5a20cd1013a4b6af5e63bd78d6e1bb
juiokval <julesvalentin04@gmail.com>
2022-05-24 14:38:21 +0200

    FROM 0-6

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
 * @h: linked list
 * Return: The number of nodes
 */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
 size_t bar;
 
@@ -12,7 +12,7 @@ bar = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
-printf("[%d] %s\n", 0, "(nil)";
+printf("[%d] %s\n", 0, "(nil)");
 else
 printf("[%d] %s\n", h->len, h->str);
 h = h->next;
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
-#include <studio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
