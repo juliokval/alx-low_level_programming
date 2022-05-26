fcf893249c55217ab990a25d7277d92ed18bbf67
juliokval <julesvalentin04@gmail.com>
2022-05-26 14:23:28 -0700

    FROM 0-last

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,68 @@
+#include "lists.h"
+/**
+  * free_listp2 - Funcion that frees a linked list
+  * @head: Head of a list
+  * Return: no return
+  */
+void free_listp2(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+/**
+  * find_listint_loop -  function that finds the loop in a linked list
+  * @h: Head of a list
+  * Return: Size of the list that was freed
+  */
+listint_t *find_listint_loop(listint_t *head)
+{
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+	listint_t *curr;
+
+	hptr = NULL;
+	while (*h != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (nnodes);
+			}
+		}
+
+		curr = *h;
+		*h = (*h)->next;
+		free(curr);
+		nnodes++;
+	}
+
+	*h = NULL;
+	free_listp2(&hptr);
+	return (nnodes);
+}
