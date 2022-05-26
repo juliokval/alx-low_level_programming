dfc03412484aba035eeebff3e6882f8dd1be739e
juliokval <julesvalentin04@gmail.com>
2022-05-26 15:16:43 -0700

    FROM 0-last

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0; i < index && head != NULL; i++)
 	{
 		head = head->next;
+		if (head == NULL)
+			return (NULL);
 	}
 	return (head);
 }
