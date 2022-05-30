fd60a011949efe7686e75fc79dd596cc6e14f5d3
juliokval <julesvalentin04@gmail.com>
2022-05-30 14:35:29 -0700

    FROM 0-7

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,13 +8,13 @@
   */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int v;
+	unsigned int x;
 
 	if (index > 63)
 		return (-1);
 
-	v = 1 << index;
-	*n = (*n | v);
+	x = 1 << index;
+	*n = (*n | x);
 
 	return (1);
 }
