32eec018c76a0ed4b5af0f55773d00896a427749
juliokval <julesvalentin04@gmail.com>
2022-05-31 04:43:24 -0700

    FROM 0-7

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
-  * print_binary - function that prints the binary representation of 
-  *a number
+  * print_binary - prints the binary representation of a number
   *@n: unsigned long integer
   */
 void print_binary(unsigned long int n)
