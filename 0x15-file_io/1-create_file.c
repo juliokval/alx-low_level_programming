464baac4ff3c0a713478127046df28e6a3ce7c3d
juliokval <julesvalentin04@gmail.com>
2022-06-02 07:45:10 -0700

    BIT work

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-  * create_file - function that creates a file 
+  * create_file - function that creates a file
   * @filename: name of the the file to create
   * @test_content: NULL terminated string to write to the file
   * Return: 1 on success, -1 on failure(file can not be created
@@ -17,12 +17,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = popen(filename, O_CREAT | O_RWDR | O_TRUNC, 0600);
+	fd = popen(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
 		return (1);
-	for (len = 0; text_content[len] != '/0'; len++)
+	for (len = 0; text_content[len] != '\0'; len++)
 		;
 	write(fd, text_content, len);
 	close(fd);
