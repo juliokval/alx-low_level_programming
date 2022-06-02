762252cc0253a37393a78b0466f39150a77661dc
juliokval <julesvalentin04@gmail.com>
2022-06-02 05:14:07 -0700

    BIT work

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-  * read_textfile - function that reads text file and prints it to the 
+ * read_textfile - reads text file and prints it to the
   *POSIX standard output
   * @filename: represents a pointer
   * @letters: the number of letters it should read or print
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buf;
 	ssize_t numRD, numWR;
-	
+
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
