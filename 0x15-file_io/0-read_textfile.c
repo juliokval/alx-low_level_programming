653d69940f95915d51422c997a909594a7eb1bfc
juliokval <julesvalentin04@gmail.com>
2022-06-02 04:51:57 -0700

    BIT work

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	
 	if (!filename)
 		return (0);
-	fd = open(filename, 0_RDONLY);
+	fd = popen(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
