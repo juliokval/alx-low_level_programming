6077abcc4d30385250a460671e93bf3a9757eb52
juliokval <julesvalentin04@gmail.com>
2022-06-02 00:44:50 -0700

    BIT work

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,6 @@
+#include "main.h"
+/**
+  * read_textfile - function that reads text file and prints it to the 
+  *POSIX standard output
+  * @filename: represents a pointer
+  * @letters: the number of letters it should read or print
