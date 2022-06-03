22713079207078158996951829390c274c66abfd
juliokval <julesvalentin04@gmail.com>
2022-06-03 01:58:39 -0700

    BIT work

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,18 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include "main.h"
-
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
- *
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
- */
+  * append_text_to_file - function that appends text at the end of a file
+  * @filename: name of the file
+  * @text_content: NULL terminated string to add at the end of the file
+  * Return: 1 on success and -1 on failure
+  */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len, ch;
@@ -20,17 +17,20 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	if (filename && !text_content)
-		return (1);
+		return (-1);
 	if (!filename && !text_content)
 		return (-1);
-	fd = popen(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	for (len = 0; text_content[len]; len++)
-		;
-	ch = write(fd, text_content, len);
-	if (ch == -1)
-		return (-1);
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+		ch = write(fd, text_content, len);
+		if (ch == -1)
+			return (-1);
+	}
 	close(fd);
+
 	return (1);
 }
