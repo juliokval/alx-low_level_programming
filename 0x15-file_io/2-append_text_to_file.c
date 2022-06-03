fedbd452706ac507c3a01bdf5b507ff119ce8c95
juliokval <julesvalentin04@gmail.com>
2022-06-03 00:38:04 -0700

    BIT work

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,18 +20,24 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+
 	if (filename && !text_content)
 		return (-1);
+
 	if (!filename && !text_content)
 		return (-1);
 	fd = open(filename, O_APPEND | O_WRONLY);
+
 	if (fd == -1)
 		return (-1);
-	for (len = 0; text_content[len]; len++)
-		;
-	ch = write(fd, text_content, len);
-	if (ch == -1)
-		return (-1);
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+		ch = write(fd, text_content, len);
+		if (ch == -1)
+			return (-1);
+	}
 	close(fd);
+
 	return (1);
 }
